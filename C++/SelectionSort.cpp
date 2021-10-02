8954648ce62ae619007f6a7e4e45d938848fcd1b
P-harry <91784662+P-harry@users.noreply.github.com>
2021-10-02 16:53:42 +0530

    Create SelectionSort.cpp

---
+++ C++/SelectionSort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+// function to print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+
+void selectionSort(int array[], int size) {
+  for (int step = 0; step < size - 1; step++) {
+    int min = step;
+    for (int i = step + 1; i < size; i++) {
+
+      if (array[i] < array[min])
+        min= i;
+    }
+
+    swap(&array[min], &array[step]);
+  }
+}
+
+
+int main() {
+  int data[] = {20, 12, 10, 15, 2};
+  int size = sizeof(data) / sizeof(data[0]);
+  selectionSort(data, size);
+  cout << "Sorted array in Acsending Order:\n";
+  printArray(data, size);
+}
+
