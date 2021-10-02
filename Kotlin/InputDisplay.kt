79292aded3b21da19190e7bf217941079a890fc0
mayorjay <joseolu4gsm@yahoo.com>
2021-10-02 12:18:36 +0100

    feat: Add Kotlin code

---
+++ Kotlin/InputDisplay.kt
@@ -0,0 +1,16 @@
+
+import java.util.Scanner
+
+public class InputDisplay {
+
+    private val scanner: Scanner = Scanner(System.`in`)
+
+    fun main() {
+        // Taking the input
+        println("Enter any String: ")
+        val input: String = scanner.nextLine()
+
+        // Displaying the input
+        print(" Your entered String is : $input")
+    }
+}
---
+++ Kotlin/PrimeNumberAdder.kt
@@ -0,0 +1,36 @@
+
+import kotlin.math.sqrt
+
+public class PrimeNumberAdder {
+
+    fun main() {
+        val myArray = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30)
+        sumPrimeNumbers(myArray);
+    }
+
+    private fun sumPrimeNumbers(numbers: IntArray) {
+        var sum = 0
+        for (num in numbers) {
+            if (isPrimeNumber(num)) {
+                sum += num
+            }
+        }
+        println(sum)
+    }
+
+    private fun isPrimeNumber(n: Int): Boolean {
+        // Exclude 0 and 1 cos they are not prime numbers
+        if (n <= 1) {
+            return false
+        }
+        // Identify the actual prime numbers
+        var i = 2
+        while (i <= sqrt(n.toDouble())) {
+            if (n % i == 0) {
+                return false
+            }
+            i++
+        }
+        return true
+    }
+}
