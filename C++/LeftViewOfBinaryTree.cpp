b46dd149ab5f365e4824a02f51c341dd00695518
Yashika <yashika.lamba.phe19@itbhu.ac.in>
2021-10-02 16:55:24 +0530

    Left View of Binary Tree added

---
+++ C++/LeftViewOfBinaryTree.cpp
@@ -0,0 +1,33 @@
+// Problem Statement: Given a Binary Tree, print Left view of it. Left view of a Binary Tree is set of nodes visible when tree is visited from Left side.
+// Problem Link: https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1
+// Solution is as follows:
+
+vector<int> leftView(Node *root)
+{
+   queue<Node*>q;
+   vector<int>v;
+    if (root == NULL)
+        return v;
+   q.push(root);
+   
+   
+   while(!q.empty())
+   {
+       int l1 = q.size();
+       int l = l1;
+    //   int i=0;
+       while (l > 0)
+       {
+           Node* temp = q.front();
+           q.pop();
+           if(l==l1)
+           v.push_back(temp->data);
+           if(temp->left)
+           q.push(temp->left);
+           if(temp->right)
+           q.push(temp->right);
+           l--;
+       }
+   }
+   return v;
+}
