d68f9325f9f8be2293f5c7c4e588a8d24473db39
Sudhanshu Patel <76786867+sudhanshupatel-dev@users.noreply.github.com>
2021-10-04 03:03:56 +0530

    Wanted to add solution from more codeforces contest

---
+++ CodeForces/Global Round 16/A. Median Maximization.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long inpt;
+    cin>>inpt;
+    for(int j=0;j<inpt;j++){
+    long long temp,sum,x;
+    cin>>temp>>sum;
+    x=sum/((temp/2)+1);cout<<x<<endl;
+    }return 0;
+    
+}
