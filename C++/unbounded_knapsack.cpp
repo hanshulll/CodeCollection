b63bbe72a04727cde1d7309b1d5ccc6610190f6f
Kunalmehta99 <46202013+Kunalmehta99@users.noreply.github.com>
2021-10-06 18:01:43 +0530

    Create unbounded_knapsack.cpp
    code and explanation

---
+++ C++/unbounded_knapsack.cpp
@@ -0,0 +1,40 @@
+// C++ program to find maximum achievable value
+// with a knapsack of weight W and multiple
+// instances allowed.
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// Returns the maximum value with knapsack of
+// W capacity
+int unboundedKnapsack(int W, int n,
+                       int val[], int wt[])
+{
+    // dp[i] is going to store maximum value
+    // with knapsack capacity i.
+    int dp[W+1];
+    memset(dp, 0, sizeof dp);
+ 
+    // Fill dp[] using above recursive formula
+    for (int i=0; i<=W; i++)
+      for (int j=0; j<n; j++)
+         if (wt[j] <= i)
+            dp[i] = max(dp[i], dp[i-wt[j]] + val[j]);
+ 
+    return dp[W];
+}
+ 
+// Driver program
+int main()
+{
+    int W = 100;
+    int val[] = {10, 30, 20};
+    int wt[] = {5, 10, 15};
+    int n = sizeof(val)/sizeof(val[0]);
+ 
+    cout << unboundedKnapsack(W, n, val, wt);
+ 
+    return 0;
+}
+Output: 
+
+300
