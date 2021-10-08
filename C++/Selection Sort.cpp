776cbc8504c6656208c637db63cc0f268da66a96
sahil-9287 <91861141+sahil-9287@users.noreply.github.com>
2021-10-08 08:57:28 +0530

    Update Selection Sort.cpp

--- C++/Selection Sort.cpp
+++ C++/Selection Sort.cpp
@@ -1,39 +1,43 @@
 #include<iostream>
 using namespace std;
-void swapping(int &a, int &b) {         //swap the content of a and b
-   int temp;
-   temp = a;
-   a = b;
-   b = temp;
+ 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
-void display(int *array, int size) {
-   for(int i = 0; i<size; i++)
-      cout << array[i] << " ";
-   cout << endl;
+ 
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+ 
+    for (i = 0; i < n-1; i++)
+    {
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+        if (arr[j] < arr[min_idx])
+            min_idx = j;
+ 
+        
+        swap(&arr[min_idx], &arr[i]);
+    }
 }
-void selectionSort(int *array, int size) {
-   int i, j, imin;
-   for(i = 0; i<size-1; i++) {
-      imin = i;   //get index of minimum data
-      for(j = i+1; j<size; j++)
-         if(array[j] < array[imin])
-            imin = j;
-         //placing in correct position
-         swap(array[i], array[imin]);
-   }
+ 
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
-int main() {
-   int n;
-   cout << "Enter the number of elements: ";
-   cin >> n;
-   int arr[n];           //create an array with given number of elements
-   cout << "Enter elements:" << endl;
-   for(int i = 0; i<n; i++) {
-      cin >> arr[i];
-   }
-   cout << "Array before Sorting: ";
-   display(arr, n);
-   selectionSort(arr, n);
-   cout << "Array after Sorting: ";
-   display(arr, n);
+ 
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    selectionSort(arr, n);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
 }
