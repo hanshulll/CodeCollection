d398b7bcbaabdc9d72ece6d8f741cb6114cf598d
Kavya Jain <89743157+ikavyajain@users.noreply.github.com>
2021-10-08 16:28:18 +0530

    Create HollowRhombusPattern.cpp
    :)

---
+++ C++/HollowRhombusPattern.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int n;  // No. of rows or columns or rhombus of size n
+cin>>n;
+for (int i=1; i<=n; i++)
+{   
+    for (int j=1; j<=n-i; j++)
+    {
+        cout<<" ";
+    }
+    for (int j=1; j<=n; j++)
+    {
+        if (i == 1 || i == n)
+        {
+            cout<<"*";
+        }
+        else if (j == 1 || j == n)
+        {
+            cout<<"*";
+        }
+        else
+        { 
+            cout<<" ";
+        }
+    }
+    cout<<endl;
+}
