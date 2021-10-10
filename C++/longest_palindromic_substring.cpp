f1631b518a08bde331598ea706146032e68113ab
kavya2008 <62183566+kavya2008@users.noreply.github.com>
2021-10-10 10:11:04 +0530

    feat/longest-palin-substring cpp

---
+++ C++/longest_palindromic_substring.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printSubStr(
+    string str, int low, int high)
+{
+    for (int i = low; i <= high; ++i)
+        cout << str[i];
+}
+int longestPalSubstr(string str)
+{
+   
+    int n = str.size();
+ 
+    bool table[n][n];
+ 
+    memset(table, 0, sizeof(table));
+ 
+    int maxLength = 1;
+ 
+    for (int i = 0; i < n; ++i)
+        table[i][i] = true;
+ 
+    .
+    int start = 0;
+    for (int i = 0; i < n - 1; ++i) {
+        if (str[i] == str[i + 1]) {
+            table[i][i + 1] = true;
+            start = i;
+            maxLength = 2;
+        }
+    }
+ 
+    for (int k = 3; k <= n; ++k) {
+        
+        for (int i = 0; i < n - k + 1; ++i) {
+            
+            int j = i + k - 1;
+ 
+            
+            if (table[i + 1][j - 1] && str[i] == str[j]) {
+                table[i][j] = true;
+ 
+                if (k > maxLength) {
+                    start = i;
+                    maxLength = k;
+                }
+            }
+        }
+    }
+ 
+    cout << "Longest palindrome substring is: ";
+    printSubStr(str, start, start + maxLength - 1);
+ 
+ 
+    return maxLength;
+}
+ 
+
+int main()
+{
+    string str = "forgeeksskeegfor";
+    cout << "\nLength is: "
+         << longestPalSubstr(str);
+    return 0;
+}
