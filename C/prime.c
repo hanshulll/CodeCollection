3f9976d32f5141b488d474e2e4fc4080df6fe64b
Akash Shanker <76723803+AkashShanker321@users.noreply.github.com>
2021-10-23 20:59:19 +0530

    Update prime.c
    Created Some Modifications and Eliminated Some Issues In The Program

--- C/prime.c
+++ C/prime.c
@@ -1,19 +1,26 @@
-#include<stdio.h>  
-int main(){    
-int n,i,m=0,flag=0;    
-printf("Enter the number to check prime:");    
-scanf("%d",&n);    
-m=n/2;    
-for(i=2;i<=m;i++)    
-{    
-if(n%i==0)    
-{    
-printf("Number is not prime");    
-flag=1;    
-break;    
-}    
-}    
-if(flag==0)    
-printf("Number is prime");     
-return 0;  
- }    
+#include <stdio.h>
+int main() {
+  int n, i, flag = 0;
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
+
+  for (i = 2; i <= n / 2; ++i) {
+    // condition for non-prime
+    if (n % i == 0) {
+      flag = 1;
+      break;
+    }
+  }
+
+  if (n == 1) {
+    printf("1 is neither prime nor composite.");
+  } 
+  else {
+    if (flag == 0)
+      printf("%d is a prime number.", n);
+    else
+      printf("%d is not a prime number.", n);
+  }
+
+  return 0;
+}
