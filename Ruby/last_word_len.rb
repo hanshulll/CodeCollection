6b02a664f11765fa30a7cbd874db3cc0ed4c000c
bidishaMondal7 <bidisha.tuli7@gmail.com>
2021-10-23 22:19:26 +0530

    Ruby program to print length of last word in a String

---
+++ Ruby/last_word_len.rb
@@ -0,0 +1,17 @@
+=begin
+    Program to print length of last word of a string
+=end
+
+# @param {String} s
+# @return {Integer}
+def length_of_last_word(s)
+    n = s.length() - 1
+    len = 0
+    while s[n] !=' '
+        len = len + 1
+        n = n -1
+    end
+    return len 
+end
+res = length_of_last_word("Hello Hacktoberfest!")
+puts(res)
