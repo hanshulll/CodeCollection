fda22d558eb7dc8314874a4a327edafa1bd27c0a
Lehnargh <andreas.lehner2@gmx.at>
2021-10-24 14:44:22 +0200

    Added an example Singleton Implementation in C#

---
+++ DesignPattern/Singleton_CSharp/SingletonPattern/SingletonPattern/ExampleSingelton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class ExampleSingleton
+    {
+        private static ExampleSingleton exampleSingleton;
+        
+        public string exampleText { get; set; }
+
+        private ExampleSingleton()
+        {
+            exampleText = "This is a example Text within the Singelton Instance.";
+        }
+
+        public static ExampleSingleton GetExampleSingeltonInstance()
+        {
+            if (exampleSingleton == null)
+            {
+                exampleSingleton = new ExampleSingleton();
+            }
+
+            return exampleSingleton;
+        }
+    }
+}
